4072c133efd7f318438423510d629db619ef9851
Derick Gutierrez <degutierrez1992@gmail.com>
2021-10-27 16:25:08 -0600

    estudiantes funcionando

---
+++ ProEquipoAzul/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextEstudiante = New System.Windows.Forms.TextBox()
+        Me.TextCodigo = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.GridView1 = New System.Windows.Forms.DataGridView()
+        Me.LabelInfo = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(444, 377)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(111, 23)
+        Me.Button3.TabIndex = 52
+        Me.Button3.Text = "Eliminar Estudiante"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(307, 377)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(113, 23)
+        Me.Button2.TabIndex = 51
+        Me.Button2.Text = "Modificar Estudiante"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(141, 377)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(147, 23)
+        Me.Button1.TabIndex = 50
+        Me.Button1.Text = "Agregar Estudiante"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextEstudiante
+        '
+        Me.TextEstudiante.Location = New System.Drawing.Point(110, 246)
+        Me.TextEstudiante.Name = "TextEstudiante"
+        Me.TextEstudiante.Size = New System.Drawing.Size(265, 20)
+        Me.TextEstudiante.TabIndex = 41
+        '
+        'TextCodigo
+        '
+        Me.TextCodigo.Location = New System.Drawing.Point(110, 216)
+        Me.TextCodigo.Name = "TextCodigo"
+        Me.TextCodigo.Size = New System.Drawing.Size(265, 20)
+        Me.TextCodigo.TabIndex = 40
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 216)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 31
+        Me.Label2.Text = "Codigo"
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(17, 246)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(57, 13)
+        Me.label1.TabIndex = 30
+        Me.label1.Text = "Estudiante"
+        '
+        'GridView1
+        '
+        Me.GridView1.AllowUserToAddRows = False
+        Me.GridView1.AllowUserToDeleteRows = False
+        Me.GridView1.AllowUserToResizeColumns = False
+        Me.GridView1.AllowUserToResizeRows = False
+        Me.GridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.GridView1.Location = New System.Drawing.Point(12, 11)
+        Me.GridView1.Name = "GridView1"
+        Me.GridView1.Size = New System.Drawing.Size(776, 188)
+        Me.GridView1.TabIndex = 29
+        '
+        'LabelInfo
+        '
+        Me.LabelInfo.AutoSize = True
+        Me.LabelInfo.Location = New System.Drawing.Point(17, 427)
+        Me.LabelInfo.Name = "LabelInfo"
+        Me.LabelInfo.Size = New System.Drawing.Size(35, 13)
+        Me.LabelInfo.TabIndex = 28
+        Me.LabelInfo.Text = "label1"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(2, 377)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(133, 23)
+        Me.Button4.TabIndex = 53
+        Me.Button4.Text = "Consultar Estudiante"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextEstudiante)
+        Me.Controls.Add(Me.TextCodigo)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.GridView1)
+        Me.Controls.Add(Me.LabelInfo)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextEstudiante As TextBox
+    Friend WithEvents TextCodigo As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents label1 As Label
+    Friend WithEvents GridView1 As DataGridView
+    Friend WithEvents LabelInfo As Label
+    Friend WithEvents Button4 As Button
+End Class
---
+++ ProEquipoAzul/Form1.vb
@@ -0,0 +1,115 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Form1
+    Public stringConeccion As String = “Data Source = DESKTOP-2ROLQTO; Initial Catalog = EstuCursos; Integrated Security = True”
+    Dim stringSelect As String = "SELECT * FROM Estudiantes "
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Call CargarGrid()
+
+    End Sub
+
+    Private Sub CargarGrid()
+
+        Dim da As SqlDataAdapter
+        Dim dt As New DataTable
+        Try
+            da = New SqlDataAdapter(stringSelect, stringConeccion)
+            da.Fill(dt)
+            Me.GridView1.DataSource = dt
+            'LabelInfo.Text = String.Format("Total datos en la tabla: {0}", dt.Rows.Count)
+            'String.Format("Total datos en la tabla: {0}", dt.Rows.Count)el 0 se sustituye por el valor de la par o que le asignes
+        Catch ex As Exception
+
+            LabelInfo.Text = "Error: " & ex.Message
+        End Try
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If TextCodigo.Text <> "" And TextEstudiante.Text <> "" Then
+                Dim InsertSql As String
+                InsertSql = "INSERT INTO Estudiantes (CodigoEstudiante, NombreEstudiante) VALUES (@CodigoEstudiante, @NombreEstudiante)"
+
+                Using con As New SqlConnection(stringConeccion)
+
+                    Dim cmd As New SqlCommand(InsertSql, con)
+                    cmd.Parameters.AddWithValue("@CodigoEstudiante", TextCodigo.Text)
+                    cmd.Parameters.AddWithValue("@NombreEstudiante", TextEstudiante.Text)
+                    con.Open()
+                    Dim t As Integer = CInt(cmd.ExecuteScalar())
+                    con.Close()
+
+                End Using
+
+                Call CargarGrid()
+                MsgBox("Ha agregado un estudiante satisfactoriamente")
+
+
+            End If
+        Catch ex As Exception
+
+            LabelInfo.Text = "Error: " & ex.Message
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            If TextEstudiante.Text <> "" Then
+
+                Dim InsertSql As String
+                InsertSql = "Delete from [dbo].[Estudiantes] where CodigoEstudiante = @CodigoEstudiante"
+
+                Using con As New SqlConnection(stringConeccion)
+
+                    Dim cmd As New SqlCommand(InsertSql, con)
+                    cmd.Parameters.AddWithValue("@CodigoEstudiante", TextCodigo.Text)
+                    con.Open()
+                    Dim t As Integer = CInt(cmd.ExecuteScalar())
+                    con.Close()
+
+                End Using
+                Call CargarGrid()
+                MsgBox("Ha eliminado un estudiante satisfactoriamente")
+
+            End If
+        Catch ex As Exception
+
+            LabelInfo.Text = "Error: " & ex.Message
+        End Try
+
+    End Sub
+
+    Private Sub GridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles GridView1.CellContentClick
+        Dim dgv As DataGridViewRow = GridView1.Rows(e.RowIndex)
+        TextCodigo.Text = dgv.Cells(0).Value.ToString()
+        TextEstudiante.Text = dgv.Cells(1).Value.ToString()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            If TextEstudiante.Text <> "" Then
+
+                Dim InsertSql As String
+                InsertSql = "Update [dbo].[Estudiantes] set NombreEstudiante = @NombreEstudiante where CodigoEstudiante = @CodigoEstudiante"
+
+                Using con As New SqlConnection(stringConeccion)
+
+                    Dim cmd As New SqlCommand(InsertSql, con)
+                    cmd.Parameters.AddWithValue("@CodigoEstudiante", TextCodigo.Text)
+                    cmd.Parameters.AddWithValue("@NombreEstudiante", TextEstudiante.Text)
+                    con.Open()
+                    Dim t As Integer = CInt(cmd.ExecuteScalar())
+                    con.Close()
+
+                End Using
+                Call CargarGrid()
+                MsgBox("Ha modificado un estudiante satisfactoriamente")
+
+            End If
+        Catch ex As Exception
+
+            LabelInfo.Text = "Error: " & ex.Message
+        End Try
+    End Sub
+End Class
